d56ddad712799e414b2371270ec6bbed4866b662
Vinaya Sree Modem <vinayasreemodem41@gmail.com>
2024-04-23 21:38:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,127 +1,137 @@
-
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { MdOutlineDelete } from "react-icons/md";
 import { GoThumbsup } from "react-icons/go";
+
 function App() {
-  const [allTodos, setAllTodos] = useState([])
-  const [newTitle, setNewTitle] = useState("")
-  const [newDesc, setNewDesc] = useState("")
-  const [allCompletedTodos, setAllCompletedTodos] = useState([])
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  const [allTodos, setAllTodos] = useState(() => JSON.parse(localStorage.getItem('todolist')) || []);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDesc, setNewDesc] = useState("");
+  const [allCompletedTodos, setAllCompletedTodos] = useState(() => JSON.parse(localStorage.getItem('completedTodolist')) || []);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => {
+        const incompleteTodos = d.filter(todo => !todo.completed);
+        const completedTodos = d.filter(todo => todo.completed);
+        setAllTodos(incompleteTodos);
+        setAllCompletedTodos(completedTodos);
+      });
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
   const handleAddTodo = () => {
-    let newTodoItem = {
+    const newTodoItem = {
       title: newTitle,
       desc: newDesc
     };
-    let updateTodo = [...allTodos];
-    updateTodo.push(newTodoItem);
+    const updateTodo = [...allTodos, newTodoItem];
     setAllTodos(updateTodo);
     localStorage.setItem('todolist', JSON.stringify(updateTodo));
-
+    setNewTitle("");
+    setNewDesc("");
   };
 
-  const handleDeleteTodo =( index,id) => {
-    let reducedTodo = [...allTodos];
-    reducedTodo.splice(index,1);
-    localStorage.setItem('todolist', JSON.stringify(reducedTodo));
-    setAllTodos(reducedTodo);
-
-  };
-  const handleCompleteTodo = (index, id) => {
-    const todoComplete = allTodos[index];
-    let reducedTodo = [...allTodos];
-
+  const handleDeleteTodo = (index, id) => {
+    const reducedTodo = [...allTodos];
     reducedTodo.splice(index, 1);
     localStorage.setItem('todolist', JSON.stringify(reducedTodo));
     setAllTodos(reducedTodo);
-    let newTodoItem = {
-      title: todoComplete.title,
-      desc: todoComplete.desc
-    };
-let updatedComplete=[...allCompletedTodos];
-    updatedComplete.push(newTodoItem);
-    setAllCompletedTodos(updatedComplete);
-    localStorage.setItem('completed todolist', JSON.stringify(updatedComplete));
-
-
   };
-  useEffect(() => {
-    let savedTodo = JSON.parse(localStorage.getItem('todolist'));
-    if (savedTodo) {
-      setAllTodos(savedTodo);
-    }
-  }, [])
-  useEffect(() => {
-    let savedTodo = JSON.parse(localStorage.getItem('completed todolist'));
-    if (savedTodo) {
-      setAllCompletedTodos(savedTodo);
-    }
-  }, [])
+
+  const handleCompleteTodo = (index) => {
+    const todoComplete = allTodos[index];
+    const reducedTodo = allTodos.filter((_, i) => i !== index);
+    const updatedComplete = [...allCompletedTodos, todoComplete];
+    setAllTodos(reducedTodo);
+    setAllCompletedTodos(updatedComplete);
+    localStorage.setItem('todolist', JSON.stringify(reducedTodo));
+    localStorage.setItem('completedTodolist', JSON.stringify(updatedComplete));
+  };
+
+  const handleDragStart = (index) => {
+    return (event) => {
+      event.dataTransfer.setData("index", index);
+    };
+  };
+
+  const handleDragOver = (index) => {
+    return (event) => {
+      event.preventDefault();
+    };
+  };
+
+  const handleDrop = (index) => {
+    return (event) => {
+      event.preventDefault();
+      const dragIndex = parseInt(event.dataTransfer.getData("index"));
+      const draggedItem = allTodos[dragIndex];
+      const updatedTodos = allTodos.filter((_, i) => i !== dragIndex);
+      updatedTodos.splice(index, 0, draggedItem);
+      setAllTodos(updatedTodos);
+      localStorage.setItem('todolist', JSON.stringify(updatedTodos));
+    };
+  };
+
   return (
     <div className="App">
       <h1> My Todos</h1>
       <div className="todo-wrapper">
         <div className="todo-inp">
           <div className="todo-input">
-
             <label>Title</label>
-            <input type="text" value={newTitle} onChange={e => setNewTitle(e.target.value)} placeholder="Whats the task title?" />
-
-
-
+            <input type="text" value={newTitle} onChange={e => setNewTitle(e.target.value)} placeholder="What's the task title?" />
           </div>
           <div className="todo-input">
-
-            <label>Description</label>
-            <input type="text" value={newDesc} onChange={e => setNewDesc(e.target.value)} placeholder="Whats the task description?" />
-
-
-
-          </div>
-          <div className="todo-input">
-
-            <button type="button" onClick={ handleAddTodo} className="primaryBtn">Add</button>
-
+            <button type="button" onClick={handleAddTodo} className="primaryBtn">Add</button>
           </div>
         </div>
       </div>
       <div className="btn-area">
-        <h1>To-do  </h1>
+        <h1>To-do</h1>
       </div>
-      <div className="todo-list" >
-        {allTodos.map((item, index) => {
-
-          return (
-            <div className="todo-list-item" key={index}>
-              <div>
-                <h3>{item.title}</h3>
-                <p>{item.desc}</p>
-              </div>
-              <div>
-                <MdOutlineDelete className='icon' onClick={() => handleDeleteTodo(index,item.id)} title="do u want to delete it" />
-                <GoThumbsup className='checkicon' onClick={() => handleCompleteTodo(index, item.id)}  title="do u want to complete it" />
-              </div>
+      <div className="todo-list">
+        {allTodos.map((item, index) => (
+          <div
+            className="todo-list-item"
+            key={index}
+            draggable
+            onDragStart={handleDragStart(index)}
+            onDragOver={handleDragOver(index)}
+            onDrop={handleDrop(index)}
+          >
+            <div>
+              <h3>{item.title}</h3>
             </div>
-          )
-        })};
-      </div>
-      <div className="todo-list" >
-        <h1>Completed </h1>
-        {allCompletedTodos.map((item, index) => {
-
-          return (
-            <div className="todo-list-item" key={index}>
-              <div>
-                <h3>{item.title}</h3>
-                <p>{item.desc}</p>
-              </div>
-              
+            <div>
+              <MdOutlineDelete className='icon' onClick={() => handleDeleteTodo(index)} title="Delete" />
+              <GoThumbsup className='checkicon' onClick={() => handleCompleteTodo(index)} title="Complete" />
             </div>
-          )
-        })};
+          </div>
+        ))}
       </div>
-    </div >
+      <div className="todo-list">
+        <h1>Completed</h1>
+        {allCompletedTodos.map((item, index) => (
+          
+          <div className="todo-list-item" key={index}
+            draggable
+            onDragStart={handleDragStart(index)}
+            onDragOver={handleDragOver(index)}
+            onDrop={handleDrop(index)}>
+            <div>
+              <h3>{item.title}</h3>
+              <p>{item.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
